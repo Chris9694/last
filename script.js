5b5a8382dabfe46bc667a58fc6cfe9215b01c808
Christian Rodríguez <christian9694@hotmail.com>
2020-06-25 18:56:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function loadPlaces(position) {
     const params = {
         radius: 300,    // search places not farther than this value (in meters)
-        clientId: '<YOUR-CLIENT-ID>',
-        clientSecret: 'YOUR-CLIENT-SECRET',
+        clientId: 'QNEBGMZBN33PMPGTY5GDCEOA1YMR5XSSFAVZUA1UKJWUOL0B',
+        clientSecret: 'TIUR0LLFBPO4P2WZ423PAY5SGUCJ2BWXLTEGYUGHXK4VVXPQ',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
 
